0068b7821baa07e9b421ac2f17d4e21095b459b9
harry <bertram_li@163.com>
2024-01-15 13:49:31 +0800

    style: go imports reviser

--- internal/etcd/client.go
+++ internal/etcd/client.go
@@ -1,9 +1,9 @@
 package etcd
 
 import (
-	"github.com/pkg/errors"
 	"time"
 
+	"github.com/pkg/errors"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"google.golang.org/grpc"
 
--- internal/scheduler/portscheduler/scheduler.go
+++ internal/scheduler/portscheduler/scheduler.go
@@ -2,10 +2,11 @@ package portscheduler
 
 import (
 	"encoding/json"
-	"github.com/pkg/errors"
 	"sort"
 	"strconv"
 	"sync"
+
+	"github.com/pkg/errors"
 
 	"github.com/mayooot/gpu-docker-api/internal/config"
 	"github.com/mayooot/gpu-docker-api/internal/etcd"
@@ -46,7 +47,7 @@ type alias struct {
 // 如果直接将 scheduler 序列化，UsedPortSet 字段以 map 的形式输出，value为 struct{}{}，而且是乱序
 func (s *scheduler) MarshalJSON() ([]byte, error) {
 	onlyKeys := make([]int, 0, len(s.UsedPortSet))
-	for k, _ := range s.UsedPortSet {
+	for k := range s.UsedPortSet {
 		onlyKeys = append(onlyKeys, k)
 	}
 	sort.Ints(onlyKeys)
